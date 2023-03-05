bdd43f32817b117db6c07829c6bf7c12ac8cd5f5
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2023-03-05 12:47:53 -0500

    .

--- ProyectoIA.pl
+++ ProyectoIA.pl
@@ -1,32 +1,25 @@
+% Luis Bravo, Camilo Garcia, Fabio Buitrago
+
+% Bases de Hechos
+
+% Se definen las conexiones entre las habitaciones h1 y h2.
 habitacion(h1).
 habitacion(h2).
+conexion(h1, h2).
+conexion(h2, h1).
 
+% Se definen las cajas de colores.
 caja(azul).
 caja(roja).
 
+% Se define la ubicacion inicial del robot y las cajas.
 ubicacion_inicial(robot, h1).
 ubicacion_inicial(caja(azul), h1).
 ubicacion_inicial(caja(roja), h1).
 
-conexion(h1, h2).
-conexion(h2, h1).
-
+% Se definen los objetivos finales del programa.
 objetivo(caja(azul), h2).
+objetivo(robot, h2).
+objetivo(caja(roja), h1).
 
-mover_robot(A, B) :-
-    ubicacion_inicial(robot, A),
-    conexion(A, B),
-    retract(ubicacion_inicial(robot, A)),
-    assertz(ubicacion_inicial(robot, B)).
-
-mover_caja(Caja, A, B) :-
-    ubicacion_inicial(Caja, A),
-    ubicacion_inicial(robot, A),
-    conexion(A, B),
-    retract(ubicacion_inicial(Caja, A)),
-    assertz(ubicacion_inicial(Caja, B)).
-
-heuristica :-
-    ubicacion_inicial(robot, H),
-    ubicacion_inicial(caja(azul), H),
-    not(ubicacion_inicial(caja(roja), H)).
+% Definicion de reglas
